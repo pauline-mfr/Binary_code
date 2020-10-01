9fe13e7e7166f0de720bc7caa097858cf1779c52
pauline-mfr <p.monfrance@codeur.online>
2020-10-01 15:27:48 +0200

    encoding display

--- main.js
+++ main.js
@@ -2,11 +2,11 @@
 document.getElementById("convert").addEventListener("click", function() {
   let code = document.getElementById('msgToCode').value;
   console.log("valeur de l'input = "+code);
+      let crypted = [];
   for (var i = 0; i < code.length; i++) {
     console.log(Number(code.charCodeAt(i)).toString(2));
-    let crypted = (Number(code.charCodeAt(i)).toString(2));
-  document.getElementById('crypted').innerHTML = crypted;
-
+     crypted.push((Number(code.charCodeAt(i)).toString(2)));
+     document.getElementById('crypted').innerHTML = crypted;
   }
 
 });
